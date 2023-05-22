3f996b5a1bf7c8aa2d58cdd7294f62da658cc98d
Yoshioka <sss.yoshioka@gmail.com>
2023-05-22 11:20:38 +0000

    initial commit

---
+++ lib/egcs.ex
@@ -0,0 +1,18 @@
+defmodule Egcs do
+  @moduledoc """
+  Documentation for `Egcs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Egcs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/egcs_test.exs
@@ -0,0 +1,8 @@
+defmodule EgcsTest do
+  use ExUnit.Case
+  doctest Egcs
+
+  test "greets the world" do
+    assert Egcs.hello() == :world
+  end
+end
